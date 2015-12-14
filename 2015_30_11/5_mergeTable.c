a0e2bfbf3785ee98d14f268dfae90bfc5471b701
HarshiShah <harshishah1607@gmail.com>
2015-12-14 21:34:07 +0530

    Progname added

--- 2015_30_11/5_mergeTable.c
+++ 2015_30_11/5_mergeTable.c
@@ -31,32 +31,62 @@ void add(struct node **head, int num){
 }
 
 /*
-n1=3 n2=5 n3=8
-res=24
+n1=3 n2=5 r=5
+res=12
 
-n1=3 n2=6 n3=5
-res=24
+n1=3 n2=6 r=5
+res=18
 
-n1=1 n2=1 n3=6
-res=6
+n1=1 n2=1 r=6
+res=7
 
-n1=3 n2=5 n3=0
+n1=3 n2=5 r=0
 res=Invalid input
+
+n1=2 n2=4 n3=6 n4=8 r=10
+res=22
+
+n1=2 n2=3 n3=5 n4=7 r=6
+res=8
 */
 int compute(int n1, int n2, int n3);
 struct node* merge(struct node *head1, struct node* head2);
+int compute2(int n1, int n2, int n3, int n4, int r);
 
 int main(){
-	int n1, n2, n3;
-	printf("\nEnter value of n1");
-	scanf("%d", &n1);
-	printf("\nEnter value of n2");
-	scanf("%d", &n2);
-	printf("\nEnter value of n3");
-	scanf("%d", &n3);
-	if (n3 > 0 && n2>0 && n1>0){
-		int res = compute(n1, n2, n3);
-		printf("\nresult=%d", res);
+	int n1, n2, r,sel;
+	printf("\nEnter number of n values(2 or 4):");
+	scanf("%d", &sel);
+	if (sel == 2 || sel == 4){
+		printf("\nEnter value of n1 :");
+		scanf("%d", &n1);
+		printf("\nEnter value of n2 :");
+		scanf("%d", &n2);
+		if (sel == 2){
+			printf("\nEnter value of r :");
+			scanf("%d", &r);
+			if (r > 0 && n2 > 0 && n1 > 0){
+				int res = compute(n1, n2, r);
+				printf("\nresult=%d", res);
+			}
+			else
+				printf("\nInvalid input");
+		}
+		else{
+			int n3, n4;
+			printf("\nEnter value of n3 :");
+			scanf("%d", &n3);
+			printf("\nEnter value of n4 :");
+			scanf("%d", &n4);
+			printf("\nEnter value of r :");
+			scanf("%d", &r);
+			if (r > 0 && n2 > 0 && n1 > 0 && n3>0 && n4>0){
+				int res = compute2(n1, n2,n3,n4, r);
+				printf("\nresult=%d", res);
+			}
+			else
+				printf("\nInvalid input");
+		}
 	}
 	else
 	printf("\nInvalid input");
@@ -85,22 +115,83 @@ struct node* merge(struct node *head1, struct node* head2){
 	return final;
 }
 
-int compute(int n1, int n2, int n3){
-	if (n1%n2 == 0){
+int compute(int n1, int n2, int r){
+	if (n1 == 1 || n2 == 1)
+		return r+1;
+	if (n1%n2 == 0 || n2%n1==0){
 		if (n2 < n1)
 			n1 = n2;
 	}
 	struct node* head1 = NULL;
-	for (int i = 1; i <= n3; i++)
+	for (int i = 1; i <= r+1; i++)
 		add(&head1, n1*i);
 	if (n1 %n2 != 0){
 		struct node* head2 = NULL;
-		for (int i = 1; i <= n3; i++)
+		for (int i = 1; i <= r+1; i++)
 			add(&head2, n2*i);
 		head1 = merge(head1, head2);
 	}
-	for (int i = 2; i <= n3; i++)
+	for (int i = 2; i <= r; i++)
 		head1 = head1->next;
-	return head1->num;
+	return head1->next->num;
 
+}
+
+int compute2(int n1, int n2, int n3, int n4, int r){
+	if (n1 == 1 || n2 == 1 || n3 == 1 || n4 == 1)
+		return r+1;
+	if (n1%n2 == 0 || n2%n1 == 0){
+		if (n2 < n1)
+			n1 = n2;
+		n2 = 0;
+	}
+	if (n1%n3 == 0 || n3%n1 == 0){
+		if (n3 < n1)
+			n1 = n3;
+		n3 = 0;
+	}
+	if (n1%n4 == 0 || n4%n1 == 0){
+		if (n4 < n1)
+			n1 = n4;
+		n4 = 0;
+	}
+	if (n2!=0 && n3!=0 && (n2%n3 == 0 || n3%n2 == 0)){
+		if (n3 < n2)
+			n2 = n3;
+		n3 = 0;
+	}
+	if (n2!=0 && n4!=0 && (n2%n4 == 0 || n4%n2 == 0)){
+		if (n4 < n2)
+			n2 = n4;
+		n4 = 0;
+	}
+	if (n3 != 0 && n4 != 0 && (n3%n4 == 0 || n4%n3 == 0)){
+		if (n4 < n3)
+			n3 = n4;
+		n4 = 0;
+	}
+	struct node* head1 = NULL;
+	for (int i = 1; i <= r+1; i++)
+		add(&head1, n1*i);
+	if (n2 != 0){
+	struct node* head2 = NULL;
+	for (int i = 1; i <= r+1; i++)
+		add(&head2, n2*i);
+		head1 = merge(head1, head2);
+	}
+	if (n3 != 0){
+		struct node* head2 = NULL;
+		for (int i = 1; i <= r+1; i++)
+			add(&head2, n3*i);
+		head1 = merge(head1, head2);
+	}
+	if (n4 != 0){
+		struct node* head2 = NULL;
+		for (int i = 1; i <= r+1; i++)
+			add(&head2, n4*i);
+		head1 = merge(head1, head2);
+	}
+	for (int i = 2; i <= r; i++)
+		head1 = head1->next;
+	return head1->next->num;
 }
--- 2015_30_11/6_sumOfLists.c
+++ 2015_30_11/6_sumOfLists.c
@@ -39,21 +39,23 @@ res=4->9->3->6
 
 list1=2
 list2=2->4
-res=4->4
+res=2->6
 
 list1=2->4
 list2=2
-res=4->4
+res=2->6
 
 list1=2->8->2->8->9->7
 list2=8->2->7->3->4->1->6->8
-res=1->1->1->0->2->3->8->6->8
+res=8->3->0->1->7->0->6->5
 
 list1=9->9->9
 list2=9->9->9
 res=1->9->9->8
 */
 struct node* sum(struct node *head1, struct node* head2);
+struct node* sum2(struct node *head1, struct node* head2);
+void reverse(struct node** head);
 
 int main(){
 	int num, len1, len2;
@@ -76,7 +78,7 @@ int main(){
 	if (len1 < 0 || len2 < 0)
 		printf("\nInvalid input");
 	else
-		head1 = sum(head1, head2);
+		head1 = sum2(head1, head2);
 	while (head1 != NULL){
 		printf("\n%d", head1->num);
 		head1 = head1->next;
@@ -85,43 +87,106 @@ int main(){
 	return 0;
 }
 
+//This function works with a double linked list
 struct node* sum(struct node *head1, struct node* head2){
 	if (head1 == NULL)
 		return head2;
 	if (head2 == NULL)
 		return head1;
-	struct node* temp = head1;
-	temp->num += head2->num;
-	head2 = head2->next;
-	//Increase list length incase of overflow
-	if (temp->num > 9){
-		struct node* temp2 = createNode(1, NULL);
-		head1->prev = temp2;
-		temp2->next = head1;
-		head1 = head1->prev;
-		temp->num = temp->num % 10;
+	struct node* temp1 = head1;
+	struct node* temp2 = head2;
+	while (temp1->next != NULL)
+		temp1 = temp1->next;
+	while (temp2->next != NULL)
+		temp2 = temp2->next;
+	int sum, carry = 0;
+	while (temp1->prev != NULL && temp2->prev != NULL){
+		sum = temp1->num + temp2->num + carry;
+		temp1->num = sum % 10;
+		carry = sum / 10;
+		temp1 = temp1->prev;
+		temp2 = temp2->prev;
 	}
-	while (temp->next != NULL && head2 != NULL){
-		temp->next->num += head2->num;
-		if (temp->next->num > 9){
-			//Increase previous values incase of a carry
-			if (temp->num < 9)
-				temp->num = temp->num + 1;
-			else{
-				struct node* temp2 = temp;
-				while (temp2->num == 9){
-					temp2->num = 0;
-					temp2 = temp2->prev;
-				}
-				temp2->num = temp2->num + 1;
-			}
-			temp->next->num = temp->next->num % 10;
+	sum = temp1->num + temp2->num + carry;
+	temp1->num = sum % 10;
+	carry = sum / 10;
+	if (temp1->prev == NULL && temp2->prev!=NULL){
+		temp1->prev = temp2->prev;
+		temp2 = temp2->prev;
+		temp2->next = temp1;
+	}
+	while (carry > 0){
+		if (temp1->prev == NULL){
+			struct node* new = createNode(carry, NULL);
+			temp1->prev = new;
+			new->next = temp1;
+			carry = 0;
 		}
-		temp = temp->next;
-		head2 = head2->next;
+		else{
+			sum = temp1->prev->num + carry;
+			temp1->prev->num = sum % 10;
+			carry = sum / 10;
+			temp1 = temp1->prev;
+		}
 	}
-	//append incase of unequal lengths
-	if (temp->next == NULL)
-		temp->next = head2;
-	return head1;
+	while (temp1->prev != NULL)
+		temp1 = temp1->prev;
+	return temp1;
 }
+
+//This function works with a single linked list and uses list reverse function
+struct node* sum2(struct node *head1, struct node* head2){
+	if (head1 == NULL)
+		return head2;
+	if (head2 == NULL)
+		return head1;
+	reverse(&head1);
+	reverse(&head2);
+	int carry = 0,sum;
+	struct node* temp1 = head1;
+	struct node* temp2 = head2;
+	while (1){
+		sum = temp1->num + temp2->num +carry;
+		temp1->num = sum%10;
+		carry = sum / 10;
+		if (temp1->next != NULL && temp2->next != NULL){
+			temp1 = temp1->next;
+			temp2 = temp2->next;
+		}
+		else
+			break;
+	}
+	if (temp1->next == NULL)
+		temp1->next = temp2->next;
+	while(carry > 0){
+		if (temp1->next == NULL){
+			temp1->next = createNode(carry, temp1);
+			carry = 0;
+		}
+		else{
+			sum = temp1->next->num + carry;
+			temp1->next->num = sum % 10;
+			carry = sum / 10;
+			temp1 = temp1->next;
+		}
+			
+	}
+	reverse(&head1);
+	return head1;
+	
+}
+
+void reverse(struct node** head){
+		struct node* part1;
+		struct node* part2;
+		if (*head == NULL)
+			return;
+		part1 = *head;
+		part2 = part1->next;
+		if (part2 == NULL)
+			return;
+		reverse(&part2);
+		part1->next->next = part1;
+		part1->next = NULL;
+		*head = part2;
+	}
